1b8b9bcfc5a005214ef4aec895e80d3128b5517b
AimJim <92112507+AimJim@users.noreply.github.com>
2025-12-16 12:44:32 +0100

    Library change

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+### Truffle Engine
+
+A game engine using Vulkan as the graphic API.
+
+
+Uses LunarXChange and SDL3
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <glm/vec3.hpp>
-
-
-int main(){
-    glm::vec3 vectornew = glm::vec3(0,1,2);
-    std::cout << vectornew.x << std::endl;
-}
-
